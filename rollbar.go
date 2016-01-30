0a1e95a50f684af691a86268ef0612c395d98cb7
kyokomi <kyoko1220adword@gmail.com>
2016-01-30 22:36:24 +0900

    Modified to able to set http.Client

--- rollbar.go
+++ rollbar.go
@@ -83,6 +83,7 @@ var (
 	waitGroup   sync.WaitGroup
 	postErrors  chan error
 	nilErrTitle = "<nil>"
+	Client = http.DefaultClient
 )
 
 // Field is a custom data field used to report arbitrary data to the Rollbar
@@ -361,7 +362,7 @@ func post(body map[string]interface{}) error {
 		return err
 	}
 
-	resp, err := http.Post(Endpoint, "application/json", bytes.NewReader(jsonBody))
+	resp, err := Client.Post(Endpoint, "application/json", bytes.NewReader(jsonBody))
 	if err != nil {
 		stderr("POST failed: %s", err.Error())
 		return err
