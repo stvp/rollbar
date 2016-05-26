61f90662fdec47bd9c4342881683b21205a5ffe4
Luke Roberts <email@luke-roberts.co.uk>
2016-05-26 14:45:24 -0700

    always close response body

--- rollbar.go
+++ rollbar.go
@@ -366,14 +366,11 @@ func post(body map[string]interface{}) error {
 		stderr("POST failed: %s", err.Error())
 		return err
 	}
+	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
 		stderr("received response: %s", resp.Status)
 		return ErrHTTPError(resp.StatusCode)
-	}
-
-	if resp != nil {
-		resp.Body.Close()
 	}
 
 	return nil
