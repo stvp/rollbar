e37477e8c69085b7db0e87c33a255cab8932a5ac
Sumeet Agarwal <sumeet@remind101.com>
2017-05-21 17:10:28 -0700

    remove fingerprint

--- stack_test.go
+++ stack_test.go
@@ -17,40 +17,6 @@ func TestBuildStack(t *testing.T) {
 	}
 }
 
-func TestStackFingerprint(t *testing.T) {
-	tests := []struct {
-		Fingerprint string
-		Stack       Stack
-	}{
-		{
-			"9344290d",
-			Stack{
-				Frame{"foo.go", "Oops", 1},
-			},
-		},
-		{
-			"a4d78b7",
-			Stack{
-				Frame{"foo.go", "Oops", 2},
-			},
-		},
-		{
-			"50e0fcb3",
-			Stack{
-				Frame{"foo.go", "Oops", 1},
-				Frame{"foo.go", "Oops", 2},
-			},
-		},
-	}
-
-	for i, test := range tests {
-		fingerprint := test.Stack.Fingerprint()
-		if fingerprint != test.Fingerprint {
-			t.Errorf("tests[%d]: got %s", i, fingerprint)
-		}
-	}
-}
-
 func TestShortenFilePath(t *testing.T) {
 	tests := []struct {
 		Given    string
